f226001df27333bcaa4174b2fe93ebc2d2d77956
chanhl22 <chanhlee@student.42seoul.kr>
2021-11-23 22:29:21 +0900

    Member 객체 & JPA 개발

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -2,7 +2,9 @@ package hellojpa;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -10,8 +12,44 @@ public class JpaMain {
         EntityManagerFactory enf = Persistence.createEntityManagerFactory("hello");
 
         EntityManager em = enf.createEntityManager();
-        em.close();
 
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+//            Member member = new Member();
+//            member.setId(2L);
+//            member.setName("HelloB");
+//
+//            em.persist(member);
+
+//            Member findMember = em.find(Member.class, 1L);
+//            System.out.println("findMember.Id = " + findMember.getId());
+//            System.out.println("findMember.Name = " + findMember.getName());
+
+//            Member findMember = em.find(Member.class, 1L);
+//
+//            em.remove(findMember);
+
+//            Member findMember = em.find(Member.class, 1L);
+//            findMember.setName("HelloJPA");
+
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(1)
+                    .setMaxResults(10)
+                    .getResultList();
+
+            for (Member member : result) {
+                System.out.println("member.name = " + member.getName());
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
         enf.close();
+
     }
 }
