2739814cd63b307d952282da44567b94fd41681b
chanhl22 <chanhlee@student.42seoul.kr>
2021-11-27 17:05:25 +0900

    기본 키 매핑(자동 할당: IDENTITY, SEQUENCE, TABLE)

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -89,12 +89,34 @@ public class JpaMain {
 //
 //            Member member2 = em.find(Member.class, 150L);
 
-            Member member = new Member();
-            member.setId(3L);
-            member.setUsername("C");
-            member.setRoleType(RoleType.GUEST);
+//            Member member = new Member();
+//            member.setUsername("C");
+//
+//            System.out.println("===================");
+//            em.persist(member);
+//            System.out.println("member.id = " + member.getId());
+//            System.out.println("===================");
 
-            em.persist(member);
+            Member member1 = new Member();
+            member1.setUsername("A");
+
+            Member member2 = new Member();
+            member2.setUsername("B");
+
+            Member member3 = new Member();
+            member3.setUsername("C");
+
+            System.out.println("===================");
+
+            em.persist(member1);
+            em.persist(member2);
+            em.persist(member3);
+
+            System.out.println("member1.id = " + member1.getId());
+            System.out.println("member2.id = " + member2.getId());
+            System.out.println("member3.id = " + member3.getId());
+
+            System.out.println("===================");
 
             tx.commit();
         } catch (Exception e) {
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -7,31 +7,61 @@ import java.util.Date;
 
 @Entity
 //@Table(uniqueConstraints = @UniqueConstraint(name = "constraintName", columnNames = {"name"}))
+@SequenceGenerator(
+        name = "MEMBER_SEQ_GENERATOR", //시퀀스 제너레이터 이름
+        sequenceName = "MEMBER_SEQ", //매핑할 데이터베이스 시퀀스 이름
+        initialValue = 1, //시작값
+        allocationSize = 50) //메모리를 통해 할당할 범위 사이즈
 public class Member {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MEMBER_SEQ_GENERATOR")
     private Long id;
 
     @Column(name = "name", nullable = false)
     private String username;
 
-    private Integer age;
+//@Entity
+//@TableGenerator(
+//        name = "MEMBER_SEQ_GENERATOR",
+//        table = "MY_SEQUENCES",
+//        pkColumnValue = "MEMBER_SEQ", allocationSize = 1)
+//public class Member {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.TABLE, generator = "MEMBER_SEQ_GENERATOR")
+//    private Long id;
+//
+//    @Column(name = "name", nullable = false)
+//    private String username;
 
-    @Enumerated(EnumType.STRING)
-    private RoleType roleType;
+//@Entity
+//public class Member {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    @Column(name = "name", nullable = false)
+//    private String username;
 
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdDate;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
-
-    private LocalDate testLocalDate;
-    private LocalDateTime testLocalDateTime;
-
-    @Lob
-    private String description;
-
+    //    private Integer age;
+//
+//    @Enumerated(EnumType.STRING)
+//    private RoleType roleType;
+//
+//    @Temporal(TemporalType.TIMESTAMP)
+//    private Date createdDate;
+//
+//    @Temporal(TemporalType.TIMESTAMP)
+//    private Date lastModifiedDate;
+//
+//    private LocalDate testLocalDate;
+//    private LocalDateTime testLocalDateTime;
+//
+//    @Lob
+//    private String description;
+//
     public Member() {
     }
 
@@ -50,46 +80,46 @@ public class Member {
     public void setUsername(String username) {
         this.username = username;
     }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public RoleType getRoleType() {
-        return roleType;
-    }
-
-    public void setRoleType(RoleType roleType) {
-        this.roleType = roleType;
-    }
-
-    public Date getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(Date createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public Date getLastModifiedDate() {
-        return lastModifiedDate;
-    }
-
-    public void setLastModifiedDate(Date lastModifiedDate) {
-        this.lastModifiedDate = lastModifiedDate;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
+//
+//    public Integer getAge() {
+//        return age;
+//    }
+//
+//    public void setAge(Integer age) {
+//        this.age = age;
+//    }
+//
+//    public RoleType getRoleType() {
+//        return roleType;
+//    }
+//
+//    public void setRoleType(RoleType roleType) {
+//        this.roleType = roleType;
+//    }
+//
+//    public Date getCreatedDate() {
+//        return createdDate;
+//    }
+//
+//    public void setCreatedDate(Date createdDate) {
+//        this.createdDate = createdDate;
+//    }
+//
+//    public Date getLastModifiedDate() {
+//        return lastModifiedDate;
+//    }
+//
+//    public void setLastModifiedDate(Date lastModifiedDate) {
+//        this.lastModifiedDate = lastModifiedDate;
+//    }
+//
+//    public String getDescription() {
+//        return description;
+//    }
+//
+//    public void setDescription(String description) {
+//        this.description = description;
+//    }
 }
 
 //package hellojpa;
