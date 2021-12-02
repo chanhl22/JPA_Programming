d090597beb2bf4cc3ccd1f86cb1c95f276185e10
chanhl22 <chanhlee@student.42seoul.kr>
2021-12-02 12:35:40 +0900

    일대일 [1:1]

--- src/main/java/hellojpa/Locker_10.java
+++ src/main/java/hellojpa/Locker_10.java
@@ -1,14 +1,16 @@
 package hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Locker_10 {
 
     @Id @GeneratedValue
+    @Column(name = "LOCKER_ID")
     private Long id;
 
     private String name;
+
+    @OneToOne (mappedBy = "locker")
+    private Member_10 member;
 }
