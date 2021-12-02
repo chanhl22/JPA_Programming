f4dcd06a2950e8a367669d63af4f7e53d90fddf6
chanhl22 <chanhlee@student.42seoul.kr>
2021-12-02 09:20:31 +0900

    일대다 [1:N]

---
+++ src/main/java/hellojpa/JpaMain_9.java
@@ -0,0 +1,38 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain_9 {
+
+    public static void main(String[] args) {
+        EntityManagerFactory enf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = enf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member_9 member = new Member_9();
+            member.setUsername("member1");
+
+            em.persist(member);
+
+            Team_9 team = new Team_9();
+            team.setName("teamA");
+            team.getMembers().add(member);
+
+            em.persist(team);
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        enf.close();
+    }
+}
